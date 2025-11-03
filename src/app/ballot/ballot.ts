fc3432e21f8e5f7f1aaf6ff7065c765d3138732f
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-11-03 22:52:50 +0100

    Move

--- src/app/ballot/ballot.ts
+++ src/app/ballot/ballot.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { Banner } from "../banner/banner";
-import { Part0 } from "../part0/part0";
-import { Part1 } from "../part1/part1";
-import { Part2 } from "../part2/part2";
-import { Part3 } from "../part3/part3";
-import { Part4 } from "../part4/part4";
-import { Part5 } from "../part5/part5";
+import { Part0 } from "../parts/part0/part0";
+import { Part1 } from "../parts/part1/part1";
+import { Part2 } from "../parts/part2/part2";
+import { Part3 } from "../parts/part3/part3";
+import { Part4 } from "../parts/part4/part4";
+import { Part5 } from "../parts/part5/part5";
 
 @Component({
     selector: 'app-ballot',
