14763ede28ca01d0edece5c28292d1fd5242838a
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-11-09 00:02:20 +0100

    Advance implementation of model1

--- src/app/parts/part0/model1.ts
+++ src/app/parts/part0/model1.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
+import { makePluralityVotingMethod } from '../../core/voting-method';
+import { SingleVoter } from '../../core/voter-group';
+import { VotingModel } from "../../play/voting-model/voting-model";
+import { Candidate } from '../../core/candidate';
+import { Voting } from '../../core/voting';
+import { Candidates } from '../../display/candidates';
 
 @Component({
     selector: 'app-model1',
-    imports: [],
+    imports: [VotingModel],
     templateUrl: './model1.html',
     styleUrl: './model1.scss',
 })
 export class Model1 {
+    readonly votingService = inject(Voting);
+    readonly candidatesDisplayService = inject(Candidates);
+
+    readonly plurality = makePluralityVotingMethod();
+    readonly candidates: Candidate[] = [
+        { shape: 'square', getOpinions: signal([50, 125]) },
+        { shape: 'triangle', getOpinions: signal([250, 125]) },
+    ]
+    readonly voterGroups = new Set([new SingleVoter(signal([125, 200]))]);
+    readonly castBallots = this.votingService.getComputedCastBallots(
+        () => this.candidates,
+        () => this.plurality,
+        () => this.voterGroups,
+    );
+    readonly winner = computed(() =>
+        Array.from(this.castBallots().values())[0]()[0]);
 }
