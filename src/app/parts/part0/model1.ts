19341b9d97020f71d0edd8d5b2433ae859fee508
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-11-08 23:05:03 +0100

    Add model1

---
+++ src/app/parts/part0/model1.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-model1',
+    imports: [],
+    templateUrl: './model1.html',
+    styleUrl: './model1.scss',
+})
+export class Model1 {
+}
--- src/app/parts/part0/part0.ts
+++ src/app/parts/part0/part0.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { Model1 } from "./model1";
 
 @Component({
   selector: 'app-part0',
-  imports: [],
+  imports: [Model1],
   templateUrl: './part0.html',
   styleUrl: './part0.scss',
 })
 export class Part0 {
-
 }
