8e4d18b5546f0cbe0e994ee80eeabad1f73a4730
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-11-08 21:21:44 +0100

    Add the candidate display service

---
+++ src/app/display/candidates.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Candidate } from '../core/candidate';
+
+const COLORS: { [k in Candidate["shape"]]: string } = {
+    square: "hsl(240, 80%, 70%)",
+    triangle: "hsl(45, 80%, 70%)",
+    hexagon: "hsl(0, 80%, 70%)",
+    pentagon: "hsl(90, 80%, 70%)",
+    circle: "hsl(30, 80%, 70%)",
+};
+
+@Injectable({
+    providedIn: 'root',
+})
+export class Candidates {
+    getColor(candidate: Candidate): string {
+        return COLORS[candidate.shape];
+    }
+
+    getImage(candidate: Candidate): string {
+        return `${candidate.shape}.svg`;
+    }
+}
