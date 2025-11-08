eb3ed3e4487317e344af0fc828028b60c5226ae8
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-11-08 23:40:55 +0100

    Take the cast ballots back, as an input

--- src/app/play/voting-model/voting-model.ts
+++ src/app/play/voting-model/voting-model.ts
@@ -3,6 +3,7 @@ import { Candidate } from "../../core/candidate";
 import { Ballot } from "../../core/ballot";
 import { SingleVoter, VoterGroup } from "../../core/voter-group";
 import { VotingMethod } from "../../core/voting-method";
+import { CastBallotSignalType } from "../../core/voting";
 
 @Component({
     selector: "app-voting-model",
@@ -14,6 +15,7 @@ export class VotingModel {
     readonly votingMethod = input.required<VotingMethod<Ballot>>();
     readonly candidates = input.required<readonly Candidate[]>();
     readonly voterGroups = input.required<ReadonlySet<VoterGroup>>();
+    readonly castBallots = input.required<ReturnType<CastBallotSignalType<Ballot>>>();
     // TODO also takes a border color as an input, and if providedn sets the border width to 10px
 
     isSingleVoter(voterGroup: VoterGroup): voterGroup is SingleVoter {
