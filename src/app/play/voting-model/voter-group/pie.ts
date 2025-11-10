af405dffa05c63e756c51ebe25aee6d86667aaa9
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-11-10 01:14:30 +0100

    Reverse the slicing direction

--- src/app/play/voting-model/voter-group/pie.ts
+++ src/app/play/voting-model/voter-group/pie.ts
@@ -15,20 +15,20 @@ export class Pie {
         this.pieShares().total ?? this.pieShares().reduce((sum, ps) => sum + ps.share, 0));
     readonly pathDs = computed(() => {
         const total = this.total();
-        const size = this.size();
+        const radius = this.size()/2;
         let currentShare = 0;
         let lastAngle = -Math.PI / 2;
         return this.pieShares().map(ps => {
             const startAngle = lastAngle;
             currentShare += ps.share;
-            const endAngle = lastAngle = (currentShare / total) * 2 * Math.PI - Math.PI / 2;
-            const x1 = (size/2) * Math.cos(startAngle);
-            const y1 = (size/2) * Math.sin(startAngle);
-            const x2 = (size/2) * Math.cos(endAngle);
-            const y2 = (size/2) * Math.sin(endAngle);
+            const endAngle = lastAngle = Math.PI * (-2 * (currentShare / total) - 0.5);
+            const x1 = radius * Math.cos(startAngle);
+            const y1 = radius * Math.sin(startAngle);
+            const x2 = radius * Math.cos(endAngle);
+            const y2 = radius * Math.sin(endAngle);
             const largeArcFlag = endAngle - startAngle > Math.PI ? 1 : 0;
             return {
-                d: `M 0 0 L ${x1} ${y1} A ${size/2} ${size/2} 0 ${largeArcFlag} 1 ${x2} ${y2} Z`,
+                d: `M 0 0 L ${x1} ${y1} A ${radius} ${radius} 0 ${largeArcFlag} 0 ${x2} ${y2} Z`,
                 color: ps.color
             };
         });
