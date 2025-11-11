74db1e899a38db4b34c98e7a375f293c504954a9
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-11-11 23:37:44 +0100

    Convert from Ecclesia's multiple-seat attribution to single-winner

---
+++ src/app/core/attribution.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@angular/core";
+import { ReadonlyCounter } from "@gouvernathor/python/collections";
+import { Candidate } from "./candidate";
+
+@Injectable({
+    providedIn: "root",
+})
+export class Attribution {
+    extractSingleWinnerFromAttribution(attribution: ReadonlyCounter<Candidate, number>): Candidate {
+        if (attribution.size !== 1) {
+            throw new Error("The attribution does not contain exactly one winner");
+        }
+        const candidate = attribution.keys().next().value!;
+        if (attribution.get(candidate) !== 1) {
+            throw new Error("The attribution does not assign exactly one seat to the winner");
+        }
+        return candidate;
+    }
+}
