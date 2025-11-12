c7d1e175ed8de0fc9be0d29e7e0f344060749f12
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-11-12 23:22:46 +0100

    Deport translations to their own file

--- src/app/parts/part0/part0.ts
+++ src/app/parts/part0/part0.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { Translations } from "./translations";
 import { Model1 } from "./model1";
 import { Model2 } from './model2';
 import { Model3 } from "./model3";
 
 @Component({
     selector: 'app-part0',
-    imports: [Model1, Model2, Model3],
+    imports: [Translations, Model1, Model2, Model3],
     templateUrl: './part0.html',
     styleUrl: './part0.scss',
 })
