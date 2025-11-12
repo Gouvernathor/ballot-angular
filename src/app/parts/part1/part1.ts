7bbd64ac188378c92341c21037d7d4074e55c93b
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-11-13 00:46:30 +0100

    whitespace

--- src/app/parts/part1/part1.ts
+++ src/app/parts/part1/part1.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-part1',
-  imports: [],
-  templateUrl: './part1.html',
-  styleUrl: './part1.scss',
+    selector: 'app-part1',
+    imports: [],
+    templateUrl: './part1.html',
+    styleUrl: './part1.scss',
 })
 export class Part1 {
-
 }
