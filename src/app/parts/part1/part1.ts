f1817f27f923634cf56495bcc11f29478609156f
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-11-13 02:22:53 +0100

    Add part1's content

--- src/app/parts/part1/part1.ts
+++ src/app/parts/part1/part1.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { BannerPart1 } from "./banner/banner";
 
 @Component({
     selector: 'app-part1',
-    imports: [],
+    imports: [BannerPart1],
     templateUrl: './part1.html',
     styleUrl: './part1.scss',
 })
