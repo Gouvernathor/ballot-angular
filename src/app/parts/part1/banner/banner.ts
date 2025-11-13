0b8cd0064147bea9ea16be46752e9f7eeb0bde96
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-11-13 01:55:04 +0100

    Add the part 1 banner

---
+++ src/app/parts/part1/banner/banner.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'h2[app-banner-part1]',
+    imports: [],
+    templateUrl: './banner.html',
+    styleUrl: './banner.scss',
+})
+export class BannerPart1 {
+}
