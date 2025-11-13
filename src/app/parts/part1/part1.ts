d099d3fc26d637eb927da94244df4fd133c87dc9
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-11-13 23:43:52 +0100

    integrate in the script

--- src/app/parts/part1/part1.ts
+++ src/app/parts/part1/part1.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { BannerPart1 } from "./banner";
+import { ModelAndBallot } from '../../play/ballot/model-and-ballot';
 
 @Component({
     selector: 'app-part1',
-    imports: [BannerPart1],
+    imports: [BannerPart1, ModelAndBallot],
     templateUrl: './part1.html',
     styleUrl: './part1.scss',
 })
