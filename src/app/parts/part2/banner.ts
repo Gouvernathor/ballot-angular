8dac895642a0e19b6f15dd0aa82b040030779ff0
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-11-14 13:36:51 +0100

    fill part 2's content

---
+++ src/app/parts/part2/banner.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'h2[app-banner-part2]',
+    imports: [],
+    templateUrl: './banner.html',
+    styleUrl: './banner.scss',
+})
+export class BannerPart2 {
+}
--- src/app/parts/part2/part2.ts
+++ src/app/parts/part2/part2.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { BannerPart2 } from './banner';
 
 @Component({
-  selector: 'app-part2',
-  imports: [],
-  templateUrl: './part2.html',
-  styleUrl: './part2.scss',
+    selector: 'app-part2',
+    imports: [BannerPart2],
+    templateUrl: './part2.html',
+    styleUrl: './part2.scss',
 })
 export class Part2 {
-
 }
