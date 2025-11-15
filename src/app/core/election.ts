6bf030b010cd09ebc794eb643e1c524d6db782ac
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-11-15 15:12:23 +0100

    Move stuff to a service

---
+++ src/app/core/election.ts
@@ -0,0 +1,112 @@
+import { Injectable, signal } from '@angular/core';
+import { Attribution as EcclesiaAttribution } from 'ecclesia/election/attribution';
+import { Ballots, Order, Scores, Simple } from 'ecclesia/election/ballots';
+import { Candidate } from './candidate';
+import { GaussianVoters } from './voter-group';
+
+export type ElectionMethodId = "FPTP" | "IRV" | "Borda" | "Condorcet" | "Approval" | "Score";
+
+interface PluralityResultInformation { // should be called FPTP ?
+    tally: Simple<Candidate>;
+    winner: Candidate;
+}
+interface IRVResultInformation {
+    tally: Order<Candidate>;
+    steps: {
+        /** Takes into account the vote reports from previously eliminated candidates */
+        firstChoicesForRemainingCandidates: Simple<Candidate>;
+        /** Either the eliminated candidate, if this is not the last round, or the winner if this is the last round */
+        selectedCandidate: Candidate;
+    }[];
+    /** Information already present as steps[-1].selectedCandidate */
+    winner: Candidate;
+}
+interface BordaResultInformation {
+    tally: Order<Candidate>;
+    /** The number of points for each candidate from each ballot, is its 0-based index */
+    processedTally: Simple<Candidate>;
+    /** The candidate with the lowest number of points */
+    winner: Candidate;
+}
+interface CondorcetResultInformation {
+    tally: Order<Candidate>;
+    pairwiseDuels: {
+        candidates: [Candidate, Candidate];
+        votesForFirst: number;
+        votesForSecond: number;
+        winner: Candidate;
+    }[];
+    duelWinsPerCandidate: Simple<Candidate>;
+    winner: Candidate|null;
+}
+interface ApprovalResultInformation {
+    tally: Simple<Candidate>;
+    winner: Candidate;
+}
+interface ScoreResultInformation {
+    tally: Scores<Candidate>;
+    /** The number of points for each candidate from each ballot, is its 1-based score */
+    processedTally: Simple<Candidate>;
+    /** Number by which to divide a processedTally value to get the average score */
+    totalVoters: number;
+    /** The number of possible scores, which is also the maximum score since it's 1-based */
+    numScores: number;
+    winner: Candidate;
+}
+
+@Injectable({
+    providedIn: 'root',
+})
+export class ElectionService {
+    makeDefaultCandidates(numCandidates: 1|2|3|4|5 = 3): Candidate[] {
+        const startAngle =
+            numCandidates === 3 ?
+                Math.PI / 6 :
+            numCandidates === 4 ?
+                Math.PI / 4 :
+            numCandidates === 5 ?
+                Math.PI / 3.3 :
+                0;
+        const radius = 100;
+        return (["square", "triangle", "hexagon", "pentagon", "bob"] as const)
+            .slice(0, numCandidates)
+            .map((shape, i) => {
+                const angle = startAngle + (i * 2 * Math.PI / numCandidates);
+                const x = 150 + radius * Math.cos(angle);
+                const y = 150 + radius * Math.sin(angle);
+                return { shape, getOpinions: signal([x, y]) };
+            });
+    }
+
+    makeDefaultVoterGroups(numVoterGroups: 1|2|3 = 1): ReadonlySet<GaussianVoters> {
+        switch (numVoterGroups) {
+            case 1:
+                return new Set([
+                    new GaussianVoters(signal([150, 150]), { num: 3 }),
+                ]);
+            case 2:
+                return new Set([
+                    new GaussianVoters(signal([150, 100]), { num: 2 }),
+                    new GaussianVoters(signal([150, 200]), { num: 2 }),
+                ]);
+            case 3:
+                return new Set([
+                    new GaussianVoters(signal([150, 115]), { num: 1 }),
+                    new GaussianVoters(signal([115, 180]), { num: 1 }),
+                    new GaussianVoters(signal([185, 180]), { num: 1 }),
+                ]);
+        }
+    }
+
+    private ecclesiaAttributionToSingleSeatAttribution<T extends Ballots<Candidate>>(
+        attribution: EcclesiaAttribution<Candidate, T>,
+    ): (tally: T) => Candidate {
+        return (tally) => {
+            const result = attribution(tally);
+            if (result.size !== 1) {
+                throw new Error(`Expected a single winner, but got ${result.size}`);
+            }
+            return result.keys().next().value!;
+        };
+    }
+}
--- src/app/play/election-model/election-model.ts
+++ src/app/play/election-model/election-model.ts
@@ -1,54 +1,11 @@
-import { Component, input, linkedSignal, signal } from '@angular/core';
-import { Candidate, CandidateShape } from '../../core/candidate';
-import { GaussianVoters } from '../../core/voter-group';
+import { Component, inject, input, linkedSignal } from '@angular/core';
+import { ElectionMethodId, ElectionService } from '../../core/election';
 
 export enum ElectionModelFeatures {
     Basic = 1,
     Voters,
     VotersAndCandidates,
     VotersAndCandidatesAndSave,
-}
-
-// election methods : FPTP, IRV, Borda, Condorcet, Approval, Score
-
-export function makeDefaultCandidates(numCandidates: 1|2|3|4|5 = 3): Candidate[] {
-    const startAngle =
-        numCandidates === 3 ?
-            Math.PI / 6 :
-        numCandidates === 4 ?
-            Math.PI / 4 :
-        numCandidates === 5 ?
-            Math.PI / 3.3 :
-            0;
-    const radius = 100;
-    return (["square", "triangle", "hexagon", "pentagon", "bob"] as const)
-        .slice(0, numCandidates)
-        .map((shape, i) => {
-            const angle = startAngle + (i * 2 * Math.PI / numCandidates);
-            const x = 150 + radius * Math.cos(angle);
-            const y = 150 + radius * Math.sin(angle);
-            return { shape, getOpinions: signal([x, y]) };
-        });
-}
-
-export function makeDefaultVoterGroups(numVoterGroups: 1|2|3 = 1): ReadonlySet<GaussianVoters> {
-    switch (numVoterGroups) {
-        case 1:
-            return new Set([
-                new GaussianVoters(signal([150, 150]), { num: 3 }),
-            ]);
-        case 2:
-            return new Set([
-                new GaussianVoters(signal([150, 100]), { num: 2 }),
-                new GaussianVoters(signal([150, 200]), { num: 2 }),
-            ]);
-        case 3:
-            return new Set([
-                new GaussianVoters(signal([150, 115]), { num: 1 }),
-                new GaussianVoters(signal([115, 180]), { num: 1 }),
-                new GaussianVoters(signal([185, 180]), { num: 1 }),
-            ]);
-    }
 }
 
 @Component({
@@ -58,10 +15,12 @@ export function makeDefaultVoterGroups(numVoterGroups: 1|2|3 = 1): ReadonlySet<G
     styleUrl: './election-model.scss',
 })
 export class ElectionModel {
-    readonly features = input(ElectionModelFeatures.Basic);
-    readonly defaultElectionMethod = input<unknown>("FPTP");
-    readonly candidates = input(makeDefaultCandidates());
-    readonly voterGroups = input(makeDefaultVoterGroups());
+    readonly electionService = inject(ElectionService);
 
-    readonly electionMethod = linkedSignal<unknown>(() => this.defaultElectionMethod());
+    readonly features = input(ElectionModelFeatures.Basic);
+    readonly defaultElectionMethod = input<ElectionMethodId>("FPTP");
+    readonly candidates = input(this.electionService.makeDefaultCandidates());
+    readonly voterGroups = input(this.electionService.makeDefaultVoterGroups());
+
+    readonly electionMethod = linkedSignal<ElectionMethodId>(() => this.defaultElectionMethod());
 }
