48699079f9c91129c70ac6c03ac163b9c57f7ae1
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-11-15 15:40:05 +0100

    create the tallies

--- src/app/play/election-model/election-model.ts
+++ src/app/play/election-model/election-model.ts
@@ -3,17 +3,13 @@ import { ElectionMethodId, ElectionService } from '../../core/election';
 import { Voting } from '../../core/voting';
 import { makeApprovalVotingMethod, makePluralityVotingMethod, makeRankedVotingMethod, makeScoreVotingMethod } from '../../core/voting-method';
 import { Candidates } from '../../display/candidates';
+import { TallyService } from '../../core/tally';
 
 export enum ElectionModelFeatures {
     Basic = 1,
     Voters,
     VotersAndCandidates,
     VotersAndCandidatesAndSave,
-}
-
-// TODO awaiting evolution on the side of VotingService
-function makeConstant<T>(value: T): () => T {
-    return () => value;
 }
 
 @Component({
@@ -24,6 +20,7 @@ function makeConstant<T>(value: T): () => T {
 })
 export class ElectionModel {
     private readonly votingService = inject(Voting);
+    private readonly tallyService = inject(TallyService);
     private readonly electionService = inject(ElectionService);
     private readonly candidateDisplayService = inject(Candidates);
 
@@ -34,27 +31,49 @@ export class ElectionModel {
 
     private readonly electionMethod = linkedSignal(() => this.defaultElectionMethod());
 
+    private readonly votingMethods = {
+        plurality: makePluralityVotingMethod(),
+        ranked: makeRankedVotingMethod(),
+        approval: makeApprovalVotingMethod(),
+        score: makeScoreVotingMethod(),
+    };
+
     // BALLOTS FOR EACH VOTING METHOD
     private readonly castPluralityBallots = this.votingService.getComputedCastBallots(
         this.candidates,
-        makeConstant(makePluralityVotingMethod()),
+        () => this.votingMethods.plurality,
         this.voterGroups,
     );
     private readonly castRankedBallots = this.votingService.getComputedCastBallots(
         this.candidates,
-        makeConstant(makeRankedVotingMethod()),
+        () => this.votingMethods.ranked,
         this.voterGroups,
     );
     private readonly castApprovalBallots = this.votingService.getComputedCastBallots(
         this.candidates,
-        makeConstant(makeApprovalVotingMethod()),
+        () => this.votingMethods.approval,
         this.voterGroups,
     );
     private readonly castScoreBallots = this.votingService.getComputedCastBallots(
         this.candidates,
-        makeConstant(makeScoreVotingMethod()),
+        () => this.votingMethods.score,
         this.voterGroups,
     );
+
+    // TALLY FOR EACH VOTING METHOD
+    private readonly pluralityTally = computed(() =>
+        this.tallyService.tallyPluralityToSimple(
+            this.votingService.extractBallots(this.castPluralityBallots)));
+    private readonly rankedTally = computed(() =>
+        this.tallyService.tallyRankedToOrder(
+            this.votingService.extractBallots(this.castRankedBallots)));
+    private readonly approvalTally = computed(() =>
+        this.tallyService.tallyApprovalToSimple(
+            this.votingService.extractBallots(this.castApprovalBallots)));
+    private readonly scoreTally = computed(() =>
+        this.tallyService.tallyScoreToScores(
+            this.votingService.extractBallots(this.castScoreBallots),
+            { maxScore: this.votingMethods.score.numScores }));
 
     // ELECTION RESULT INFORMATION FOR EACH ELECTION METHOD
     private readonly fptpResultInformation = computed(() =>
