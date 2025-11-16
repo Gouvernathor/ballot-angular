d917bfe413c56b5a2c0d3ab906b0a8bb6a07bb41
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-11-16 19:57:18 +0100

    add part 3's banner

---
+++ src/app/parts/part3/banner.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'h2[app-banner-part3]',
+    imports: [],
+    templateUrl: './banner.html',
+    styleUrl: './banner.scss',
+})
+export class BannerPart3 {
+}
--- src/app/parts/part3/part3.ts
+++ src/app/parts/part3/part3.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { BannerPart3 } from "./banner";
 
 @Component({
   selector: 'app-part3',
-  imports: [],
+  imports: [BannerPart3],
   templateUrl: './part3.html',
   styleUrl: './part3.scss',
 })
 export class Part3 {
-
 }
