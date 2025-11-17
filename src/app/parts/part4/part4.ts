e66121d2724cf1571a887ea95c7191184e68a440
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-11-17 20:11:00 +0100

    integrate the election model inside

--- src/app/parts/part4/part4.ts
+++ src/app/parts/part4/part4.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { ElectionModel } from "../../play/election-model/election-model";
 
 @Component({
-  selector: 'app-part4',
-  imports: [],
-  templateUrl: './part4.html',
-  styleUrl: './part4.scss',
+    selector: 'app-part4',
+    imports: [ElectionModel],
+    templateUrl: './part4.html',
+    styleUrl: './part4.scss',
 })
 export class Part4 {
-
 }
