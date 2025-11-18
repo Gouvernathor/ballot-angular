45d0a14a6a7b24eb850440f87fc1bdcc16c825c4
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-11-18 01:37:33 +0100

    Add the sandbox page and its resolvers

---
+++ src/app/+sandbox/sandbox.resolvers.ts
@@ -0,0 +1,42 @@
+import { ActivatedRouteSnapshot, ResolveFn } from "@angular/router";
+import { ElectionModelFeatures } from "../play/election-model/election-model";
+import { Opinions } from "../core/candidate";
+
+interface SandboxResolver {
+    description: ResolveFn<string|undefined>;
+    features: ResolveFn<ElectionModelFeatures.FullSandbox>;
+    defaultElectionMethod: ResolveFn<string|undefined>;
+    defaultCandidates: ResolveFn<readonly Opinions[]|undefined>;
+    defaultVoterGroups: ResolveFn<readonly Opinions[]|undefined>;
+}
+
+function extractOpinionsArray(route: ActivatedRouteSnapshot, ...keys: string[]): readonly Opinions[]|undefined {
+    const qpm = route.queryParamMap;
+    for (const key of keys) {
+        const values = qpm.getAll(key);
+        if (values.length > 0) {
+            return values
+                .map(v => v.split(",").map(val => Number(val)))
+                .filter((opinions): opinions is [number, number] => opinions.length === 2) as Opinions[];
+        }
+    }
+    return undefined;
+}
+
+export const sandboxResolvers: SandboxResolver = {
+    description: (route) => {
+        const qpm = route.queryParamMap;
+        return qpm.get("d") ?? qpm.get("description") ?? undefined;
+    },
+    features: () => ElectionModelFeatures.FullSandbox,
+    defaultElectionMethod: (route) => {
+        const qpm = route.queryParamMap;
+        return qpm.get("s") ?? qpm.get("electionMethod") ?? undefined;
+    },
+    defaultCandidates: (route) => {
+        return extractOpinionsArray(route, "c", "candidates");
+    },
+    defaultVoterGroups: (route) => {
+        return extractOpinionsArray(route, "v", "voters");
+    },
+};
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { Index } from './+index';
+import { ElectionModel } from './play/election-model/election-model';
+import { sandboxResolvers } from './+sandbox/sandbox.resolvers';
 
 export const routes: Routes = [{
     path: '',
     component: Index,
+}, {
+    path: 'sandbox',
+    component: ElectionModel,
+    resolve: sandboxResolvers,
 }];
