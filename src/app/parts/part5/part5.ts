2b8a4ed1c062ed54ec508eb5b4a10d590ce09190
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-11-18 23:48:18 +0100

    chexk in incomplete contents and styling for the last part

--- src/app/parts/part5/part5.ts
+++ src/app/parts/part5/part5.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-part5',
-  imports: [],
-  templateUrl: './part5.html',
-  styleUrl: './part5.scss',
+    selector: 'app-part5',
+    imports: [],
+    templateUrl: './part5.html',
+    styleUrl: './part5.scss',
 })
 export class Part5 {
-
 }
