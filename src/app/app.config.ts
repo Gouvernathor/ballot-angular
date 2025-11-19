cd1207c938a887cc6b7ed66af1d8c12b254ef090
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-11-19 22:27:40 +0100

    Add transloco

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,29 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+import { TranslocoHttpLoader } from './transloco-loader';
+import { provideTransloco } from '@jsverse/transloco';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes, withComponentInputBinding()), provideClientHydration(withEventReplay())
+    provideRouter(routes, withComponentInputBinding()),
+    provideClientHydration(withEventReplay()),
+
+    provideHttpClient(),
+    provideTransloco({
+      config: {
+        availableLangs: ['en', 'fr'],
+        defaultLang: 'en',
+        // Remove this option if your application doesn't support changing language in runtime.
+        // reRenderOnLangChange: true,
+        prodMode: !isDevMode(),
+      },
+      loader: TranslocoHttpLoader,
+    }),
   ]
 };
