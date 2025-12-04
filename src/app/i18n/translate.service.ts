7aad6833b5953cd7dc61c3d9bd2eed1fb3f11466
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-12-04 23:37:57 +0100

    Front service

---
+++ src/app/i18n/translate.service.ts
@@ -0,0 +1,37 @@
+import { computed, inject, Injectable, Signal } from '@angular/core';
+import { SupportedLanguage, TranslationsRepository } from './translations.repository';
+import { LanguageService } from './language.service';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class TranslateService {
+    private readonly translationsRepository = inject(TranslationsRepository);
+    private readonly languageService = inject(LanguageService);
+
+    doesKeyExist(key: string, lang: SupportedLanguage = this.languageService.currentLanguage()): boolean {
+        const store = this.translationsRepository.mainStore[lang];
+        const val = store[key];
+        return typeof val === "string";
+    }
+
+    private getTranslation(key: string, lang: SupportedLanguage): string | undefined {
+        let store = this.translationsRepository.mainStore[lang];
+        while (key) {
+            const value = store[key];
+            if (typeof value === "string") {
+                return value;
+            } else if (typeof value === "object" && value !== null) {
+                key = key.split(".", 2)[1];
+                store = value as any;
+            } else {
+                return undefined;
+            }
+        }
+        return undefined;
+    }
+
+    getTranslationSignal(key: string, lang?: SupportedLanguage): Signal<string|undefined> {
+        return computed(() => this.getTranslation(key, lang ?? this.languageService.currentLanguage()));
+    }
+}
