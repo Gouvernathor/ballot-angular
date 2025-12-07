b5b99bc899d86bd70884a9a825e6b01989e1670d
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-12-07 13:46:45 +0100

    fix import

--- src/app/tl/fr-FR/+index/index.ts
+++ src/app/tl/fr-FR/+index/index.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { Index } from '../../../+index';
 import { Banner } from "../../../banner/banner";
-import { Nav } from "../../../parts/part0/nav";
+import { Nav } from '../../../nav/nav';
 import { FrFrPart0 } from "../parts/part0/part0";
 
 @Component({
     selector: 'app-fr-fr-index',
-    imports: [Banner, FrFrPart0, Nav],
+    imports: [Banner, Nav, FrFrPart0],
     templateUrl: './index.html',
     styleUrls: [],
 })
