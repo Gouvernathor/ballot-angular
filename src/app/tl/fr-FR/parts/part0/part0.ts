685302aa15154ebee66aebf9923b32c92659eaad
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-12-07 13:36:27 +0100

    remove style file for tl part0

--- src/app/tl/fr-FR/parts/part0/part0.ts
+++ src/app/tl/fr-FR/parts/part0/part0.ts
@@ -5,7 +5,7 @@ import { Part0 } from '../../../../parts/part0/part0';
     selector: 'app-fr-fr-part0',
     imports: [],
     templateUrl: './part0.html',
-    styleUrls: ['../../../../parts/part0/part0.scss', './part0.scss'],
+    styleUrls: ['../../../../parts/part0/part0.scss'],
 })
 export class FrFrPart0 extends Part0 {
 }
