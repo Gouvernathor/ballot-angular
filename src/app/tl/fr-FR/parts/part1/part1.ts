c71161187e96081b729269e849d3021054d4ec62
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-12-07 23:25:09 +0100

    Translate part 1 banner

--- src/app/tl/fr-FR/parts/part1/part1.ts
+++ src/app/tl/fr-FR/parts/part1/part1.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Part1 } from '../../../../parts/part1/part1';
+import { FrFrBannerPart1 } from "./banner";
 
 @Component({
     selector: 'app-fr-fr-part1',
-    imports: [],
+    imports: [FrFrBannerPart1],
     templateUrl: './part1.html',
     styleUrls: ['../../../../parts/part1/part1.scss'],
 })
