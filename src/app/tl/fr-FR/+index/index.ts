8a95c9ce688a119e784e42410c6f060a755a248f
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-12-07 13:41:25 +0100

    add the nav to the translation

--- src/app/tl/fr-FR/+index/index.ts
+++ src/app/tl/fr-FR/+index/index.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Index } from '../../../+index';
 import { Banner } from "../../../banner/banner";
+import { Nav } from "../../../parts/part0/nav";
 import { FrFrPart0 } from "../parts/part0/part0";
 
 @Component({
     selector: 'app-fr-fr-index',
-    imports: [Banner, FrFrPart0],
+    imports: [Banner, FrFrPart0, Nav],
     templateUrl: './index.html',
     styleUrls: [],
 })
