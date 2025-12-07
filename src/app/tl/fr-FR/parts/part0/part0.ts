6f86395323dd25fc7ca155fbfde960192dd71528
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-12-07 21:46:01 +0100

    Add and translate up to model2

--- src/app/tl/fr-FR/parts/part0/part0.ts
+++ src/app/tl/fr-FR/parts/part0/part0.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Part0 } from '../../../../parts/part0/part0';
 import { Model1 } from "../../../../parts/part0/model1";
+import { Model2 } from "../../../../parts/part0/model2";
 
 @Component({
     selector: 'app-fr-fr-part0',
-    imports: [Model1],
+    imports: [Model1, Model2],
     templateUrl: './part0.html',
     styleUrls: ['../../../../parts/part0/part0.scss'],
 })
