67184db780c97b2900d47d57df7bc2b40061ae15
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-12-07 23:12:44 +0100

    Fix semantics for the first banner

--- src/app/parts/part1/banner.ts
+++ src/app/parts/part1/banner.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'h2[app-banner-part1]',
+    selector: 'hgroup[app-banner-part1]',
     imports: [],
     templateUrl: './banner.html',
     styleUrl: './banner.scss',
