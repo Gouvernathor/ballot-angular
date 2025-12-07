d040b8efcd6bb6c8b372c23d9743e66470f41057
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-12-07 02:12:59 +0100

    Add the first inherited part

--- src/app/tl/fr-FR/+index/index.ts
+++ src/app/tl/fr-FR/+index/index.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Banner } from "../../../banner/banner";
+import { FrFrPart0 } from "../parts/part0/part0";
 
 @Component({
     selector: 'app-fr-fr-index',
-    imports: [Banner],
+    imports: [Banner, FrFrPart0],
     templateUrl: './index.html',
     styleUrl: './index.scss',
 })
---
+++ src/app/tl/fr-FR/parts/part0/part0.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Part0 } from '../../../../parts/part0/part0';
+
+@Component({
+    selector: 'app-fr-fr-part0',
+    imports: [],
+    templateUrl: './part0.html',
+    styleUrls: ['../../../../parts/part0/part0.scss', './part0.scss'],
+})
+export class FrFrPart0 extends Part0 {
+}
