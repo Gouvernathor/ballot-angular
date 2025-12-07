497235d6513698b891cc65a3060919272fe45390
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-12-07 22:09:13 +0100

    translate up to model3

--- src/app/tl/fr-FR/parts/part0/part0.ts
+++ src/app/tl/fr-FR/parts/part0/part0.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Part0 } from '../../../../parts/part0/part0';
 import { Model1 } from "../../../../parts/part0/model1";
 import { Model2 } from "../../../../parts/part0/model2";
+import { Model3 } from "../../../../parts/part0/model3";
 
 @Component({
     selector: 'app-fr-fr-part0',
-    imports: [Model1, Model2],
+    imports: [Model1, Model2, Model3],
     templateUrl: './part0.html',
     styleUrls: ['../../../../parts/part0/part0.scss'],
 })
