aaa60fab1fd718638f2af610a46ed06e81f82ee2
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-12-07 13:03:19 +0100

    better (nav) semantics for the pseudo-"translations" section

--- src/app/parts/part0/translations.ts
+++ src/app/parts/part0/translations.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-translations',
+    selector: '[app-translations]',
     imports: [],
     templateUrl: './translations.html',
     styleUrl: './translations.scss',
