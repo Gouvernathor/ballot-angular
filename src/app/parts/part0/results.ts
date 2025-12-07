bbf433f5ebfb2c9b1884a8969f132a9eddbd897c
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-12-07 21:55:07 +0100

    localize part0 results

--- src/app/parts/part0/results.ts
+++ src/app/parts/part0/results.ts
@@ -1,7 +1,8 @@
-import { Component, inject, input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { Candidate } from '../../core/candidate';
 import { FPTPResultInformation } from '../../core/election';
 import { CandidatesDisplayService } from '../../display/candidates';
+import { LANG } from '../../i18n/language.service';
 
 @Component({
     selector: 'app-results-part0',
@@ -11,9 +12,19 @@ import { CandidatesDisplayService } from '../../display/candidates';
 })
 export class ResultsComponentPart0 {
     readonly candidateDisplayService = inject(CandidatesDisplayService);
+    readonly lang = inject(LANG);
 
     readonly candidates = input.required<readonly Candidate[]>();
     readonly results = input.required<FPTPResultInformation>();
 
     readonly verbose = input(false);
+
+    readonly winner = computed(() =>
+        this.results().winner);
+    readonly winnerColor = computed(() =>
+        this.candidateDisplayService.getColor(this.winner()));
+
+    getName(candidate: Candidate) {
+        return this.candidateDisplayService.getLocalizedName(candidate, this.lang);
+    }
 }
