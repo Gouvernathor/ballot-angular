b6e6d9723fdfa615c4c62d57bf3d4d5bacc8f8a7
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-12-08 00:50:59 +0100

    add the ballot1 to translation

--- src/app/tl/fr-FR/parts/part1/part1.ts
+++ src/app/tl/fr-FR/parts/part1/part1.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Part1 } from '../../../../parts/part1/part1';
 import { FrFrBannerPart1 } from "./banner";
+import { ModelAndBallot } from "../../../../play/ballot/model-and-ballot";
 
 @Component({
     selector: 'app-fr-fr-part1',
-    imports: [FrFrBannerPart1],
+    imports: [FrFrBannerPart1, ModelAndBallot],
     templateUrl: './part1.html',
     styleUrls: ['../../../../parts/part1/part1.scss'],
 })
