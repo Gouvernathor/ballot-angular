d9e5adcb4c2db6f7f90337e506926596f90c275f
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-12-07 22:42:34 +0100

    Create french part 1

--- src/app/tl/fr-FR/+index/index.ts
+++ src/app/tl/fr-FR/+index/index.ts
@@ -3,10 +3,11 @@ import { Index } from '../../../+index';
 import { Banner } from "../../../banner/banner";
 import { Nav } from '../../../nav/nav';
 import { FrFrPart0 } from "../parts/part0/part0";
+import { FrFrPart1 } from "../parts/part1/part1";
 
 @Component({
     selector: 'app-fr-fr-index',
-    imports: [Banner, Nav, FrFrPart0],
+    imports: [Banner, Nav, FrFrPart0, FrFrPart1],
     templateUrl: './index.html',
     styleUrls: [],
 })
---
+++ src/app/tl/fr-FR/parts/part1/part1.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Part1 } from '../../../../parts/part1/part1';
+
+@Component({
+    selector: 'app-fr-fr-part1',
+    imports: [],
+    templateUrl: './part1.html',
+    styleUrls: ['../../../../parts/part1/part1.scss'],
+})
+export class FrFrPart1 extends Part1 {
+}
