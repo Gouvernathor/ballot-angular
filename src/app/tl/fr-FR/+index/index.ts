7edb8769217e31c4d60a90b11bd3a8fe54fd85c8
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-12-07 01:49:51 +0100

    Add the banner

--- src/app/tl/fr-FR/+index/index.ts
+++ src/app/tl/fr-FR/+index/index.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { Banner } from "../../../banner/banner";
 
 @Component({
-  selector: 'app-fr-fr-index',
-  imports: [],
-  templateUrl: './index.html',
-  styleUrl: './index.scss',
+    selector: 'app-fr-fr-index',
+    imports: [Banner],
+    templateUrl: './index.html',
+    styleUrl: './index.scss',
 })
 export class Index {
-
 }
