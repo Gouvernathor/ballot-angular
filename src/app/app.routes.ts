89bb96cf58ae7aca56346819c4a500c57f1f6336
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-12-07 01:43:33 +0100

    Create french parallel index

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,29 @@ import { Routes } from '@angular/router';
 import { Index } from './+index';
 import { Sandbox } from './+sandbox/sandbox';
 import { sandboxResolvers } from './+sandbox/sandbox.resolvers';
+import { Index as FrFrIndex } from './tl/fr-FR/+index';
 
 export const routes: Routes = [
     {
         path: '',
         component: Index,
-    }, {
+    },
+    {
         path: 'sandbox',
         component: Sandbox,
         resolve: sandboxResolvers,
+    },
+
+    // other language routes : the structure should be the same as the routes above
+    {
+        path: "fr-FR",
+        // data: { lang: "fr-FR" },
+        children: [
+            {
+                path: "",
+                component: FrFrIndex,
+            },
+            // TODO sandbox (same resolvers)
+        ],
     },
 ];
---
+++ src/app/tl/fr-FR/+index/index.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fr-fr-index',
+  imports: [],
+  templateUrl: './index.html',
+  styleUrl: './index.scss',
+})
+export class Index {
+
+}
