275c5a102ba98f252cb1125cdb9e15bbc9dfdf19
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-12-08 22:06:16 +0100

    simplify some

--- src/app/play/election-result/fptp-result.ts
+++ src/app/play/election-result/fptp-result.ts
@@ -1,7 +1,8 @@
-import { Component, inject, input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { Candidate } from '../../core/candidate';
 import { CandidatesDisplayService } from '../../display/candidates';
 import { FPTPResultInformation } from '../../core/election';
+import { LANG } from '../../i18n/language.service';
 
 @Component({
     selector: 'app-fptp-result',
@@ -10,8 +11,17 @@ import { FPTPResultInformation } from '../../core/election';
     styleUrls: ["./election-results.scss", './fptp-result.scss'],
 })
 export class FPTPResult {
-    readonly candidateDisplayService = inject(CandidatesDisplayService);
+    readonly candidatesDisplayService = inject(CandidatesDisplayService);
+    readonly lang = inject(LANG);
 
     readonly candidates = input.required<readonly Candidate[]>();
     readonly results = input.required<FPTPResultInformation>();
+
+    private readonly winner = computed(() => this.results().winner);
+    readonly winnerColor = computed(() =>
+        this.candidatesDisplayService.getColor(this.winner()));
+    readonly winnerSrc = computed(() =>
+        this.candidatesDisplayService.getIconImage(this.winner()));
+    readonly winnerName = computed(() =>
+        this.candidatesDisplayService.getLocalizedName(this.winner().shape, this.lang));
 }
