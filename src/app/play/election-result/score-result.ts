494918cedf0fa04ca7577d2569a3fa06db1d088f
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-12-08 23:01:22 +0100

    localize the score results

--- src/app/play/election-result/score-result.ts
+++ src/app/play/election-result/score-result.ts
@@ -1,8 +1,9 @@
-import { Component, inject, input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { DecimalPipe } from '@angular/common';
 import { Candidate } from '../../core/candidate';
 import { ScoreResultInformation } from '../../core/election';
 import { CandidatesDisplayService } from '../../display/candidates';
+import { LANG } from '../../i18n/language.service';
 
 @Component({
     selector: 'app-score-result',
@@ -12,7 +13,16 @@ import { CandidatesDisplayService } from '../../display/candidates';
 })
 export class ScoreResult {
     readonly candidatesDisplayService = inject(CandidatesDisplayService);
+    readonly lang = inject(LANG);
 
     readonly candidates = input.required<readonly Candidate[]>();
     readonly results = input.required<ScoreResultInformation>();
+
+    private readonly winner = computed(() => this.results().winner);
+    readonly winnerColor = computed(() =>
+        this.candidatesDisplayService.getColor(this.winner()));
+    readonly winnerSrc = computed(() =>
+        this.candidatesDisplayService.getIconImage(this.winner()));
+    readonly winnerName = computed(() =>
+        this.candidatesDisplayService.getLocalizedName(this.winner().shape, this.lang));
 }
