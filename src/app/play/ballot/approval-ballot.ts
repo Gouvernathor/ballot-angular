5c5f8bc4a5dee0acf8157568096d78e06c2b80fd
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-12-08 02:16:16 +0100

    localize the approval ballot

--- src/app/play/ballot/approval-ballot.ts
+++ src/app/play/ballot/approval-ballot.ts
@@ -1,6 +1,8 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { ApprovalBallot } from '../../core/ballot';
 import { CandidateShape } from '../../core/candidate';
+import { CandidatesDisplayService } from '../../display/candidates';
+import { LANG } from '../../i18n/language.service';
 
 @Component({
     selector: 'app-approval-ballot',
@@ -9,11 +11,18 @@ import { CandidateShape } from '../../core/candidate';
     styleUrls: ["./common-ballots.scss", './approval-ballot.scss'],
 })
 export class ApprovalBallotComponent {
+    readonly candidateDisplayService = inject(CandidatesDisplayService);
+    readonly lang = inject(LANG);
+
     readonly ballot = input.required<ApprovalBallot>();
 
     src(shape: CandidateShape) {
         const ballot = this.ballot();
         const checked = Array.from(ballot, s => s.shape).includes(shape);
         return checked ? "play/ballot/checked.webp" : "play/ballot/unchecked.webp";
+    }
+
+    getFullName(shape: CandidateShape): string {
+        return this.candidateDisplayService.getLocalizedFullName(shape, this.lang);
     }
 }
