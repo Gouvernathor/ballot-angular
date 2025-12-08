5aafe63dfb8d453b7c922f5517822ac9abaa3d06
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-12-08 01:17:34 +0100

    localize ranked ballot

--- src/app/play/ballot/ranked-ballot.ts
+++ src/app/play/ballot/ranked-ballot.ts
@@ -1,6 +1,8 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { RankedBallot } from '../../core/ballot';
 import { CandidateShape } from '../../core/candidate';
+import { CandidatesDisplayService } from '../../display/candidates';
+import { LANG } from '../../i18n/language.service';
 
 @Component({
     selector: 'app-ranked-ballot',
@@ -9,11 +11,18 @@ import { CandidateShape } from '../../core/candidate';
     styleUrls: ["./common-ballots.scss", './ranked-ballot.scss'],
 })
 export class RankedBallotComponent {
+    readonly candidateDisplayService = inject(CandidatesDisplayService);
+    readonly lang = inject(LANG);
+
     readonly ballot = input.required<RankedBallot>();
 
     src(shape: CandidateShape) {
         const ballot = this.ballot();
         const index = ballot.findIndex(c => c.shape === shape);
         return `play/ballot/${index+1}.webp`;
+    }
+
+    getFullName(shape: CandidateShape): string {
+        return this.candidateDisplayService.getLocalizedFullName(shape, this.lang);
     }
 }
