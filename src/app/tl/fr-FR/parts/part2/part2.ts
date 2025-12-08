d2ef0e998fa7292501576e05e571769765af1e66
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-12-08 23:01:56 +0100

    translate up to election1

--- src/app/tl/fr-FR/parts/part2/part2.ts
+++ src/app/tl/fr-FR/parts/part2/part2.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Part2 } from '../../../../parts/part2/part2';
 import { FrFrBannerPart2 } from './banner';
+import { ElectionModel } from "../../../../play/election-model/election-model";
 
 @Component({
   selector: 'app-fr-fr-part2',
-  imports: [FrFrBannerPart2],
+  imports: [FrFrBannerPart2, ElectionModel],
   templateUrl: './part2.html',
   styleUrls: ['../../../../parts/part2/part2.scss'],
 })
