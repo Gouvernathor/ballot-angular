2f458b84b0ce864ca542217e048c40a40af53013
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-12-08 22:52:25 +0100

    localize condorcet results

--- src/app/play/election-result/condorcet-result.ts
+++ src/app/play/election-result/condorcet-result.ts
@@ -1,7 +1,8 @@
-import { Component, inject, input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { CandidatesDisplayService } from '../../display/candidates';
 import { Candidate } from '../../core/candidate';
 import { CondorcetResultInformation } from '../../core/election';
+import { LANG } from '../../i18n/language.service';
 
 @Component({
     selector: 'app-condorcet-result',
@@ -11,7 +12,10 @@ import { CondorcetResultInformation } from '../../core/election';
 })
 export class CondorcetResult {
     readonly candidatesDisplayService = inject(CandidatesDisplayService);
+    readonly lang = inject(LANG);
 
     readonly candidates = input.required<readonly Candidate[]>();
     readonly results = input.required<CondorcetResultInformation>();
+
+    readonly winner = computed(() => this.results().winner);
 }
