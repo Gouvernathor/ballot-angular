6d7ce8374af8dd4ff9ec2ab1880d1986425ac2ec
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-12-08 22:55:12 +0100

    localize approval results

--- src/app/play/election-result/approval-result.ts
+++ src/app/play/election-result/approval-result.ts
@@ -1,7 +1,8 @@
-import { Component, inject, input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { ApprovalResultInformation } from '../../core/election';
 import { Candidate } from '../../core/candidate';
 import { CandidatesDisplayService } from '../../display/candidates';
+import { LANG } from '../../i18n/language.service';
 
 @Component({
     selector: 'app-approval-result',
@@ -11,7 +12,16 @@ import { CandidatesDisplayService } from '../../display/candidates';
 })
 export class ApprovalResult {
     readonly candidatesDisplayService = inject(CandidatesDisplayService);
+    readonly lang = inject(LANG);
 
     readonly candidates = input.required<readonly Candidate[]>();
     readonly results = input.required<ApprovalResultInformation>();
+
+    private readonly winner = computed(() => this.results().winner);
+    readonly winnerColor = computed(() =>
+        this.candidatesDisplayService.getColor(this.winner()));
+    readonly winnerSrc = computed(() =>
+        this.candidatesDisplayService.getIconImage(this.winner()));
+    readonly winnerName = computed(() =>
+        this.candidatesDisplayService.getLocalizedName(this.winner().shape, this.lang));
 }
