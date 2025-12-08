f0ac0ba1ceeb083dd57e6c7d7da6716710039a9c
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-12-08 22:37:35 +0100

    localize borda results

--- src/app/play/election-result/borda-result.ts
+++ src/app/play/election-result/borda-result.ts
@@ -1,7 +1,8 @@
-import { Component, inject, input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { CandidatesDisplayService } from '../../display/candidates';
 import { BordaResultInformation } from '../../core/election';
 import { Candidate } from '../../core/candidate';
+import { LANG } from '../../i18n/language.service';
 
 @Component({
     selector: 'app-borda-result',
@@ -11,7 +12,16 @@ import { Candidate } from '../../core/candidate';
 })
 export class BordaResult {
     readonly candidatesDisplayService = inject(CandidatesDisplayService);
+    readonly lang = inject(LANG);
 
     readonly candidates = input.required<readonly Candidate[]>();
     readonly results = input.required<BordaResultInformation>();
+
+    private readonly winner = computed(() => this.results().winner);
+    readonly winnerColor = computed(() =>
+        this.candidatesDisplayService.getColor(this.winner()));
+    readonly winnerSrc = computed(() =>
+        this.candidatesDisplayService.getIconImage(this.winner()));
+    readonly winnerName = computed(() =>
+        this.candidatesDisplayService.getLocalizedName(this.winner().shape, this.lang));
 }
