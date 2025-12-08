8e054b825892b7a9d14434e6f4e85ac35e0d661b
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-12-08 19:36:31 +0100

    Add banner

--- src/app/tl/fr-FR/parts/part2/part2.ts
+++ src/app/tl/fr-FR/parts/part2/part2.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Part2 } from '../../../../parts/part2/part2';
+import { FrFrBannerPart2 } from './banner';
 
 @Component({
   selector: 'app-fr-fr-part2',
-  imports: [],
+  imports: [FrFrBannerPart2],
   templateUrl: './part2.html',
   styleUrls: ['../../../../parts/part2/part2.scss'],
 })
