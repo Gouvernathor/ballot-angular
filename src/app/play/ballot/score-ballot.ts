b4b808cbc3d4f6cf68264467eca519af69e96c0e
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-12-08 02:27:45 +0100

    localize score ballot

--- src/app/play/ballot/score-ballot.ts
+++ src/app/play/ballot/score-ballot.ts
@@ -1,5 +1,8 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { ScoreBallot } from '../../core/ballot';
+import { CandidateShape } from '../../core/candidate';
+import { CandidatesDisplayService } from '../../display/candidates';
+import { LANG } from '../../i18n/language.service';
 
 @Component({
     selector: 'app-score-ballot',
@@ -8,6 +11,9 @@ import { ScoreBallot } from '../../core/ballot';
     styleUrls: ["./common-ballots.scss", './score-ballot.scss'],
 })
 export class ScoreBallotComponent {
+    readonly candidateDisplayService = inject(CandidatesDisplayService);
+    readonly lang = inject(LANG);
+
     readonly ballot = input.required<ScoreBallot>();
 
     src(shape: string) {
@@ -15,5 +21,13 @@ export class ScoreBallotComponent {
         const score = Array.from(ballot, ([c, score]) => ({ s: c.shape, score }))
             .find(({ s }) => s === shape)!.score;
         return `play/ballot/range${score}.webp`;
+    }
+
+    getName(shape: CandidateShape): string {
+        return this.candidateDisplayService.getLocalizedName(shape, this.lang);
+    }
+
+    getFullName(shape: CandidateShape): string {
+        return this.candidateDisplayService.getLocalizedFullName(shape, this.lang);
     }
 }
