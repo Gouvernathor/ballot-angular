73ff633d1a5fa3630b4a67555db4b51313ead1b4
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-12-08 22:27:42 +0100

    localize IRV results

--- src/app/play/election-result/irv-result.ts
+++ src/app/play/election-result/irv-result.ts
@@ -1,7 +1,8 @@
-import { Component, inject, input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { CandidatesDisplayService } from '../../display/candidates';
 import { IRVResultInformation } from '../../core/election';
 import { Candidate } from '../../core/candidate';
+import { LANG } from '../../i18n/language.service';
 
 @Component({
     selector: 'app-irv-result',
@@ -11,7 +12,16 @@ import { Candidate } from '../../core/candidate';
 })
 export class IrvResult {
     readonly candidatesDisplayService = inject(CandidatesDisplayService);
+    readonly lang = inject(LANG);
 
     readonly candidates = input.required<readonly Candidate[]>();
     readonly results = input.required<IRVResultInformation>();
+
+    private readonly winner = computed(() => this.results().winner);
+    readonly winnerColor = computed(() =>
+        this.candidatesDisplayService.getColor(this.winner()));
+    readonly winnerSrc = computed(() =>
+        this.candidatesDisplayService.getIconImage(this.winner()));
+    readonly winnerName = computed(() =>
+        this.candidatesDisplayService.getLocalizedName(this.winner().shape, this.lang));
 }
