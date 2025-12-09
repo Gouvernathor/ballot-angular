da9ee1a0e09a669bafebf797f63db84471bd7011
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-12-09 01:10:47 +0100

    translate part 3 banner

---
+++ src/app/tl/fr-FR/parts/part3/banner.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { BannerPart3 } from '../../../../parts/part3/banner';
+
+@Component({
+  selector: 'hgroup[app-banner-fr-fr-part3]',
+  imports: [],
+  templateUrl: './banner.html',
+  styleUrls: ['../../../../parts/part3/banner.scss'],
+})
+export class FrFrBannerPart3 extends BannerPart3 {
+}
--- src/app/tl/fr-FR/parts/part3/part3.ts
+++ src/app/tl/fr-FR/parts/part3/part3.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Part3 } from '../../../../parts/part3/part3';
+import { FrFrBannerPart3 } from "./banner";
 
 @Component({
     selector: 'app-fr-fr-part3',
-    imports: [],
+    imports: [FrFrBannerPart3],
     templateUrl: './part3.html',
     styleUrls: ['../../../../parts/part3/part3.scss'],
 })
