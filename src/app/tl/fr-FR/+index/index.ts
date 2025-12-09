8f6998bc18ad5e91f69ec6f79a00068eabfdfd28
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-12-09 01:07:07 +0100

    create part 3

--- src/app/tl/fr-FR/+index/index.ts
+++ src/app/tl/fr-FR/+index/index.ts
@@ -5,10 +5,11 @@ import { Nav } from '../../../nav/nav';
 import { FrFrPart0 } from "../parts/part0/part0";
 import { FrFrPart1 } from "../parts/part1/part1";
 import { FrFrPart2 } from '../parts/part2/part2';
+import { FrFrPart3 } from '../parts/part3/part3';
 
 @Component({
     selector: 'app-fr-fr-index',
-    imports: [Banner, Nav, FrFrPart0, FrFrPart1, FrFrPart2],
+    imports: [Banner, Nav, FrFrPart0, FrFrPart1, FrFrPart2, FrFrPart3],
     templateUrl: './index.html',
     styleUrls: [],
 })
---
+++ src/app/tl/fr-FR/parts/part3/part3.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Part3 } from '../../../../parts/part3/part3';
+
+@Component({
+    selector: 'app-fr-fr-part3',
+    imports: [],
+    templateUrl: './part3.html',
+    styleUrls: ['../../../../parts/part3/part3.scss'],
+})
+export class FrFrPart3 extends Part3 {
+}
