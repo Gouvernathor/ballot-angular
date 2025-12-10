cf2e8794d51ff9c0eef7ef9323c9ad1efe78c0f0
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-12-10 21:24:28 +0100

    Include local translations in the nav

--- src/app/nav/nav.ts
+++ src/app/nav/nav.ts
@@ -1,13 +1,28 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
+import { LANG, SupportedLanguage } from '../i18n/language.service';
+import { RouterLink } from "@angular/router";
+
+interface LocalTranslation {
+    routerLink: string;
+    label: string;
+    lang: SupportedLanguage;
+}
 
 @Component({
     selector: '[app-nav]',
-    imports: [],
+    imports: [RouterLink],
     templateUrl: './nav.html',
     styleUrl: './nav.scss',
 })
 export class Nav {
+    readonly lang = inject(LANG);
+
     readonly originalLabel = input("Original");
     readonly translationsLabel = input("Translations:");
     readonly sourceCodeLabel = input("Full source code here!");
+
+    readonly localTranslations: readonly LocalTranslation[] = [
+        { routerLink: "", label: "English", lang: "en-CA" },
+        { routerLink: "/fr-FR", label: "Français", lang: "fr-FR" },
+    ];
 }
