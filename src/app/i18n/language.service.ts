f0967231f497ac75a305bab2702054472245b7fa
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-12-11 00:39:29 +0100

    Reshape the injectable for the browser's preferred language

--- src/app/i18n/language.service.ts
+++ src/app/i18n/language.service.ts
@@ -1,4 +1,4 @@
-import { DOCUMENT, inject, Injectable, InjectionToken, signal } from '@angular/core';
+import { DOCUMENT, inject, InjectionToken, signal } from '@angular/core';
 
 const supportedLanguages = [
     "en-CA",
@@ -8,6 +8,7 @@ const supportedLanguages = [
 ] as const;
 export type SupportedLanguage = (typeof supportedLanguages)[number];
 
+
 /**
  * The current language.
  * Set for every route by the router.
@@ -15,35 +16,39 @@ export type SupportedLanguage = (typeof supportedLanguages)[number];
  */
 export const LANG = new InjectionToken<SupportedLanguage>("LANG");
 
-@Injectable({
-    providedIn: 'root',
-})
-export class LanguageService {
-    private readonly window = inject(DOCUMENT).defaultView;
 
-    private browserChosenLanguage(): SupportedLanguage | undefined {
-        const nav = this.window?.navigator;
-
-        if (nav?.languages?.length) {
-            for (const lang of nav.languages) {
-                const bare = lang.split("-")[0];
-                const matched = supportedLanguages.find(supported =>
-                    supported === lang || supported.split("-")[0] === bare);
-                if (matched) {
-                    return matched;
-                }
+function getBrowserChosenLanguage(nav: Navigator): SupportedLanguage|undefined {
+    if (nav?.languages?.length) {
+        for (const lang of nav.languages) {
+            const bare = lang.split("-")[0];
+            const matched = supportedLanguages.find(supported =>
+                supported === lang || supported.split("-")[0] === bare);
+            if (matched) {
+                return matched;
             }
         }
+    }
 
-        const language = nav?.language;
-        const bare = language?.split("-")[0];
-        return supportedLanguages.find(supported =>
-            supported === language || supported.split("-")[0] === bare);
-    }
-    private readonly browserLanguage = signal(this.browserChosenLanguage());
-    constructor() {
-        this.window?.addEventListener("languagechange", () => {
-            this.browserLanguage.set(this.browserChosenLanguage());
-        });
-    }
+    const language = nav?.language;
+    const bare = language?.split("-")[0];
+    return supportedLanguages.find(supported =>
+        supported === language || supported.split("-")[0] === bare);
 }
+function makeBrowserPrefLangSignal(): () => SupportedLanguage|undefined {
+    const window = inject(DOCUMENT).defaultView;
+    if (window) {
+        const nav = window.navigator;
+
+        const langSignal = signal(getBrowserChosenLanguage(nav));
+        window.addEventListener("languagechange", () => {
+            langSignal.set(getBrowserChosenLanguage(nav));
+        });
+
+        return langSignal;
+    }
+    return () => undefined;
+}
+
+export const BROWSER_PREF_LANG = new InjectionToken<() => SupportedLanguage|undefined>("BROWSER_PREF_LANG", {
+    factory: makeBrowserPrefLangSignal,
+});
