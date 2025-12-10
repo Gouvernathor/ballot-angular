a1d44eec5f50ef1cdaa0d0b133eea89f0c865c04
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-12-10 21:36:32 +0100

    repatriate the queryparams in the template

--- src/app/+sandbox/sandbox.ts
+++ src/app/+sandbox/sandbox.ts
@@ -20,12 +20,4 @@ export class Sandbox {
     });
     readonly defaultCandidates = input.required<readonly Opinions[]|undefined>();
     readonly defaultVoterGroups = input.required<readonly Opinions[]|undefined>();
-
-    // Because of the quote " around BOB, this can't be written inline in the HTML
-    readonly queryParams4 = {
-        s: "IRV",
-        v: [[150, 150]],
-        c: [[197, 149], [150, 104], [150, 195], [106, 149], [150, 152]],
-        d: `IRV's Silly Non-Monotonicity: Move Circle ("BOB") around the other 4 candidates, and watch how the results flip chaotically between them, but never to Bob!`,
-    };
 }
