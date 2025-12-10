caa4621bc9e6c697b8435e667ba5c3f70e9dbef4
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-12-10 20:22:21 +0100

    Retrofit it in the english sandbox

--- src/app/+sandbox/sandbox.ts
+++ src/app/+sandbox/sandbox.ts
@@ -2,10 +2,11 @@ import { Component, computed, input } from '@angular/core';
 import { ElectionModel, ElectionModelFeatures } from "../play/election-model/election-model";
 import { Opinions } from '../core/candidate';
 import { isElectionMethodId } from '../core/election';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-sandbox',
-    imports: [ElectionModel],
+    imports: [ElectionModel, RouterLink],
     templateUrl: './sandbox.html',
     styleUrl: './sandbox.scss',
 })
@@ -19,4 +20,12 @@ export class Sandbox {
     });
     readonly defaultCandidates = input.required<readonly Opinions[]|undefined>();
     readonly defaultVoterGroups = input.required<readonly Opinions[]|undefined>();
+
+    // Because of the quote " around BOB, this can't be written inline in the HTML
+    readonly queryParams4 = {
+        s: "IRV",
+        v: [[150, 150]],
+        c: [[197, 149], [150, 104], [150, 195], [106, 149], [150, 152]],
+        d: `IRV's Silly Non-Monotonicity: Move Circle ("BOB") around the other 4 candidates, and watch how the results flip chaotically between them, but never to Bob!`,
+    };
 }
