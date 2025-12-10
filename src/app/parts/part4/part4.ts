6d7a28e15a0bc07b4fd5a6211b2d2548d8c183da
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-12-10 20:30:13 +0100

    Adapt the link in part4
    which is actually the first from sandbox

--- src/app/parts/part4/part4.ts
+++ src/app/parts/part4/part4.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ElectionModel } from "../../play/election-model/election-model";
+import { RouterLink } from "@angular/router";
 
 @Component({
     selector: 'app-part4',
-    imports: [ElectionModel],
+    imports: [ElectionModel, RouterLink],
     templateUrl: './part4.html',
     styleUrl: './part4.scss',
 })
--- src/app/tl/fr-FR/parts/part4/part4.ts
+++ src/app/tl/fr-FR/parts/part4/part4.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Part4 } from '../../../../parts/part4/part4';
 import { ElectionModel } from "../../../../play/election-model/election-model";
+import { RouterLink } from "@angular/router";
 
 @Component({
     selector: 'app-fr-fr-part4',
-    imports: [ElectionModel],
+    imports: [ElectionModel, RouterLink],
     templateUrl: './part4.html',
     styleUrls: ['../../../../parts/part4/part4.scss'],
 })
