ce887a3a2af50b108ce120b6babe251b29665127
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-12-10 15:26:25 +0100

    Translate part 5

--- src/app/tl/fr-FR/parts/part5/part5.ts
+++ src/app/tl/fr-FR/parts/part5/part5.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Part5 } from '../../../../parts/part5/part5';
+import { RouterLink } from "@angular/router";
 
 @Component({
     selector: 'app-fr-fr-part5',
-    imports: [],
+    imports: [RouterLink],
     templateUrl: './part5.html',
-    styleUrls: ['../../../../parts/part5/part5.scss'],
+    styleUrls: ['../../../../parts/part5/part5.scss', './part5.scss'],
 })
 export class FrFrPart5 extends Part5 {
 }
