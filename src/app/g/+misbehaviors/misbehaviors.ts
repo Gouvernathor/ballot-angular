294aada22d449994bba4e89bf7c943875e230035
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-12-29 22:39:06 +0100

    simplify unnecessary sub-element

--- src/app/g/+misbehaviors/misbehaviors.ts
+++ src/app/g/+misbehaviors/misbehaviors.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { Banner } from "../../banner/banner";
-import { BannerInfighting } from "./banner-infighting";
 import { BannerBlackmail } from "./banner-blackmail";
 
 @Component({
-    imports: [Banner, BannerInfighting, BannerBlackmail],
+    imports: [Banner, BannerBlackmail],
     templateUrl: './misbehaviors.html',
     styleUrl: './misbehaviors.scss',
 })
